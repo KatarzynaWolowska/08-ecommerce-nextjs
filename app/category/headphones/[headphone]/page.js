6f9f82187e5a6c24f18ee58812d173f52c95ed94
Katarzyna Wolowska <wolowska.katarzyna@gmail.com>
2023-11-14 14:06:26 +0100

    cleanup, first commit

---
+++ app/category/headphones/[headphone]/page.js
@@ -0,0 +1,5 @@
+const Headphone = ({ params }) => {
+    return <>Headphone: {params.headphone}</>
+}
+
+export default Headphone
---
+++ app/category/headphones/page.js
@@ -0,0 +1,5 @@
+const Headphones = () => {
+    return <>Headphones</>
+}
+
+export default Headphones
