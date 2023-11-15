abc8d0ecc086b7e37b33bc509f8e437dd0d52b83
Katarzyna Wolowska <wolowska.katarzyna@gmail.com>
2023-11-15 23:35:36 +0100

    ADD: TopBanner, breakpoints; FIX: button

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import styles from './Button.module.scss'
 
-const Button = ({ children, variant, onClick, ...props }) => {
+const Button = ({ children, variant = 'contained', onClick, ...props }) => {
     return (
         <button className={styles[variant]} onClick={onClick} {...props}>
             {children}
