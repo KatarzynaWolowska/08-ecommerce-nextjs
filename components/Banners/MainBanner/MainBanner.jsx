ed46573baa34b19a71ac1aa3e7d487d22cbb755f
Katarzyna Wolowska <wolowska.katarzyna@gmail.com>
2023-11-23 19:14:01 +0100

    ADD: circle aniamtion

--- components/Banners/MainBanner/MainBanner.jsx
+++ components/Banners/MainBanner/MainBanner.jsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import Button from '@/components/Button/Button'
-import Wrapper from '@/components/Wrapper/Wrapper'
 import styles from './MainBanner.module.scss'
+import circle from './circleAnimation.module.scss'
 import photo from '../../../public/banner-speaker-zx9.png'
 
 const MainBanner = () => {
     return (
         <section className={styles.mainBanner}>
+            <div className={circle.animation}>
+                <div className={circle.animation1} />
+                <div className={circle.animation2} />
+                <div className={circle.animation3} />
+            </div>
             <div className={styles.column}>
                 <Image src={photo} className={styles.photo} alt='ZX9 speaker' />
                 <h2 className='h1'>
