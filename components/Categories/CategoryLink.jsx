f478b93771fe965ff40e12900c83cb56b6098b38
Katarzyna Wolowska <wolowska.katarzyna@gmail.com>
2023-11-28 19:39:48 +0100

    FIX: CategoryLink styles

--- components/Categories/CategoryLink.jsx
+++ components/Categories/CategoryLink.jsx
@@ -1,22 +1,23 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from './Categories.module.scss'
+
+import BUTTON_VARIANTS from '@/constants/ButtonVariant'
+
 import shadow from '../../public/category-shadow.png'
 import Button from '../Button/Button'
+import styles from './Categories.module.scss'
 
 const CategoryLink = ({ title, href, image }) => {
     return (
         <Link href={href}>
             <div className={styles.photoBox}>
-                <Image src={shadow} className={styles.shadow} alt='' />
                 <Image src={image} className={styles.photo} alt={title} />
+                <Image src={shadow} className={styles.shadow} alt='' />
             </div>
-            <div className={styles.grayBox}>
-                <p className='h6'>{title}</p>
-                <Button variant='link' style={{ margin: 0 }}>
-                    Shop
-                </Button>
-            </div>
+            <p className='h6'>{title}</p>
+            <Button variant={BUTTON_VARIANTS.ghost} style={{ margin: 0 }}>
+                Shop
+            </Button>
         </Link>
     )
 }
