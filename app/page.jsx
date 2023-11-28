d483f46e9b4cca24a53bc4f69b146e228ce22878
Katarzyna Wolowska <wolowska.katarzyna@gmail.com>
2023-11-28 21:22:18 +0100

    FIX: button styles

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
+import Playground from '@/components/Playground/Playground'
 import TopBanner from '@/components/Banners/TopBanner'
-import Button from '@/components/Button/Button'
-import Wrapper from '@/components/Wrapper/Wrapper'
 
 const Home = () => {
     return (
         <>
             <TopBanner />
+            <Playground />
         </>
     )
 }
